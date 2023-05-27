4f75ec8972ea2475ef8a16bd5c6bcca41c7aca57
Oğuzhan YAVUZ <79802391+0gu2h4n@users.noreply.github.com>
2023-05-27 16:36:36 +0300

    Create main.py
    Main iskeleti

---
+++ main.py
@@ -0,0 +1,10 @@
+from Insan import Insan
+from Issiz import Issiz
+from Calisan import Calisan
+from MaviYaka import MaviYaka
+from BeyazYaka import BeyazYaka
+
+def main():
+    pass
+
+if __name__ == "__main__": main()  
