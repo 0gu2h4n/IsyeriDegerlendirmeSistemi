22d392bbc7b73e6f571f510b233120522916a537
Oğuzhan YAVUZ <79802391+0gu2h4n@users.noreply.github.com>
2023-05-27 15:14:35 +0300

    Update Issiz.py
    Tecrübesiz statüsü eklendi
    Statüler isteğe göre sıralandı
    Düzeltmeler

--- Issiz.py
+++ Issiz.py
@@ -3,7 +3,7 @@ from Insan import Insan
 class Issiz(Insan):
     def __init__(self, tc_no:str, ad:str, soyad:str, yas:int, cinsiyet:str, uyruk:str, maviYaka:float = 0.0, beyazYaka:float = 0.0, yonetici:float=0.0) -> None:
         super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
-        self._tecrube = {"maviyaka" : maviYaka, "beyazyaka" : beyazYaka, "yonetici" : yonetici}
+        self._tecrube = {"maviyaka" : maviYaka, "beyazyaka" : beyazYaka, "yönetici" : yonetici}
         self.statu = self.statu_bul()
 
     def tecrube_ekle(self, statu:str, yil:int) -> None:
@@ -20,12 +20,14 @@ class Issiz(Insan):
             print("Hatalı statü girişi yapıldı. Lütfen statüyü kontrol ediniz.")
 
     def statu_bul(self) -> str:
-        if self._tecrube["maviyaka"] * 0.2 > self._tecrube["beyazyaka"] * 0.35 and self._tecrube["maviyaka"] * 0.2 > self._tecrube["yonetici"] * 0.45:
-            return "maviyaka"
-        elif self._tecrube["beyazyaka"] * 0.35 > self._tecrube["maviyaka"] * 0.2 and self._tecrube["beyazyaka"] * 0.35 > self._tecrube["yonetici"] * 0.45:
-            return "beyazyaka"
-        else:
-            return "yonetici"
-
+        if not self._tecrube["yönetici"] and not self._tecrube["beyazyaka"] and not self._tecrube["maviyaka"]:
+            return "Tecrübesiz"
+        elif self._tecrube["yönetici"] * 0.45 >= self._tecrube["maviyaka"] * 0.2 and self._tecrube["yönetici"] * 0.45 >= self._tecrube["beyazyaka"] * 0.35:
+            return "Yönetici"
+        elif self._tecrube["beyazyaka"] * 0.35 > self._tecrube["yönetici"] * 0.45 and self._tecrube["beyazyaka"] * 0.35 >= self._tecrube["maviyaka"] * 0.2:
+            return "Beyaz Yaka"
+        elif self._tecrube["maviyaka"] * 0.2 > self._tecrube["yönetici"] * 0.45 and self._tecrube["maviyaka"] * 0.2 > self._tecrube["beyazyaka"] * 0.35:
+            return "Mavi Yaka"
+        
     def __str__(self) -> str:
         return f"Ad: {self._ad}\nSoyad: {self._soyad}\nStatü: {self.statu_bul()}\n"
