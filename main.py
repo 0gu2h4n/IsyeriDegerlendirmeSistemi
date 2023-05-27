756ce0ba6eeea679773fd1457ba072ce5106c09b
Oğuzhan YAVUZ <79802391+0gu2h4n@users.noreply.github.com>
2023-05-27 18:28:11 +0300

    İnsan seçim
    İnsan menü seçimi ve kayıt işlemleri

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ from MaviYaka import MaviYaka
 from BeyazYaka import BeyazYaka
 
 def main():
-    pass
+    while True:
+        print("\n***Çalışan Bilgi Sistemi***\n")
+        kayitlar = []
+        secim = input("Kayıt türü seçiniz:\n1-İnsan\n2-İşsiz\n3-Çalışan\n4-Beyaz Yaka\n5-Mavi Yaka\n0-Çıkış\nSeçiminiz: ")
+        if secim == "1":
+            _tcno = input("TC Kimlik No: ")
+            _ad = input("Ad: ")
+            _soyad = input("Soyad: ")
+            _yas = int(input("Yaş: "))
+            _cinsiyet = input("Cinsiyet: ")
+            _uyruk = input("Uyruk: ")
+            insan = Insan(_tcno, _ad, _soyad, _yas, _cinsiyet, _uyruk)
+            kayitlar.append(insan)
+
+
 
 if __name__ == "__main__": main()  
