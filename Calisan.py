d9db46ecb1678ac4c0b8c331516bdd2583482c04
Oğuzhan YAVUZ <79802391+0gu2h4n@users.noreply.github.com>
2023-05-27 16:13:19 +0300

    Accessor Modifier + yenimaas()
    Accessor Modifier yazım hatası düzeltme
    Yeni maaş fonksiyonu

--- Calisan.py
+++ Calisan.py
@@ -4,42 +4,44 @@ class Calisan(Insan):
     def __init__(self, tc_no: str, ad: str, soyad: str, yas: int, cinsiyet: str, uyruk: str, sektor:str, tecrube:int, maas:float) -> None:
         super().__init__(tc_no, ad, soyad, yas, cinsiyet, uyruk)
         if sektor in _sektorler:
-            self._sektor = sektor
+            self.__sektor = sektor
         else:
             raise Exception("Sektör bilgisi hatalı.")
-        self._tecrube = tecrube
-        self._maas = maas
+        self._Calisan__tecrube = tecrube
+        self._Calisan__maas = maas
 
     def sektor_yap(self, sektor:str) -> None:
         if sektor in _sektorler:
-            self._sektor = sektor
+            self.__sektor = sektor
         else:
             raise Exception("Sektör bilgisi hatalı.")
         
     def sektor_al(self) -> str:
-        return self._sektor
+        return self.__sektor
 
     def tecrube_yap(self, tecrube:int) -> None:
-        self._tecrube = tecrube
+        self._Calisan__tecrube = tecrube
 
     def tecrube_al(self) -> int:
-        return self._tecrube
+        return self._Calisan__tecrube
 
     def maas_yap(self, maas:float) -> None:
-        self._maas = maas
+        self._Calisan__maas = maas
 
     def maas_al(self) -> float:
-        return self._maas
+        return self._Calisan__maas
     
     def zam_hakki(self) -> float:
-        if self._tecrube < 24:
+        if self._Calisan__tecrube < 24:
             return 0
-        elif self._tecrube >= 24 and self._tecrube < 48 and self._maas < 15000:
-            return (self._maas % self._tecrube)
-        elif self._tecrube >= 48 and self._maas < 25000:
-            return (self._maas % self._tecrube) / 2
+        elif self._Calisan__tecrube >= 24 and self._Calisan__tecrube < 48 and self._Calisan__maas < 15000:
+            return (self._Calisan__maas % self._Calisan__tecrube)
+        elif self._Calisan__tecrube >= 48 and self._Calisan__maas < 25000:
+            return (self._Calisan__maas % self._Calisan__tecrube) / 2
         else:
             return 0
         
+    def yenimaas(self): return self._Calisan__maas * (1 + self.zam_hakki() / 100)
+        
     def __str__(self) -> str:
-        return f"Ad: {self._ad}\nSoyad: {self._soyad}\nTecrübe: {self._tecrube}\nYeni Maaş: {self._maas * (1 + self.zam_hakki() / 100) :.2f}\n"
+        return f"Ad: {self._Insan__ad}\nSoyad: {self._Insan__soyad}\nTecrübe: {self._Calisan__tecrube}\nYeni Maaş: {self.yenimaas():.2f}\n"
