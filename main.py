2a1a9c6c07d43774020fee9c0bda31f2b5527d2f
Oğuzhan YAVUZ <79802391+0gu2h4n@users.noreply.github.com>
2023-05-27 16:38:08 +0300

    İşsizler
    İşsizler oluşturuldu

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ def main():
     print(insan1)
     print(insan2)
 
+    issiz1 = Issiz("10003", "Ayşe", "Aras", 28, "Kadın", "Türk", 6, 1, 0)
+    issiz2 = Issiz("10004", "Selim", "Duran", 39, "Kadın", "Türk", 7, 4, 4)
+    issiz3 = Issiz("10005", "Mehmet", "Diren", 25, "Erkek", "Türk", 3, 2, 2)
+    print(issiz1)
+    print(issiz2)
+    print(issiz3)
+
 
 
 if __name__ == "__main__": main()  
